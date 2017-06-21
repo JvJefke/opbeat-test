140b416f8805e82620088493ac960eb821e91a12
Thomas Bormans <thomas.bormans@district01.be>
2017-06-21 14:21:50 +0200

    Core routes moved to seperate route file

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,52 +1,10 @@
 require("rootpath")();
-const config = require("config");
 const glob = require("glob");
-
-const CoreController = require("server/controllers/core");
 
 module.exports = (app) => {
 	glob.sync("server/routes/!(index).js").forEach(route => {
 		require(route)(app);
 	});
-
-	/**
-	 * @swagger
-	 * /status:
-	 *   get:
-	 *     description: Status call of the server
-	 *     produces:
-	 *       - application/json
-	 *     parameters:
-	 *       - name: success
-	 *         description: Success boolean
-	 *         in: body
-	 *         required: true
-	 *         type: boolean
-	 *       - name: version
-	 *         description: App version
-	 *         in: body
-	 *         required: true
-	 *         type: string
-	 *     responses:
-	 *       200:
-	 *         description: OK
-	 */
-	app.route("/status").get(CoreController.status);
-
-	if (config.state.docs) { // Only allow docs if configured
-		/**
-		 * @swagger
-		 * /docs:
-		 *   get:
-		 *     description: Documentation of all API's
-		 *     produces:
-		 *       - text/html
-		 *     responses:
-		 *       200:
-		 *         description: OK
-		 */
-		app.route("/docs").get(CoreController.docs);
-	}
 
 	// Fallback route
 	app.route(["/", "/*"]).all((req, res) => {
