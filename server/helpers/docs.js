934ad311a9089cdbbd06060eee9ac6613b0cb1c3
Thomas Bormans <thomas.bormans@district01.be>
2017-07-13 08:16:01 +0200

    Creating swagger json to middleware

--- server/helpers/docs.js
+++ server/helpers/docs.js
@@ -1,21 +1,5 @@
 require("rootpath")();
-const glob = require("glob");
-const swaggerJSDoc = require("swagger-jsdoc");
 const prettySwag = require("pretty-swag");
-const packageJson = require("package.json");
-
-// Set options for Swagger
-const options = {
-	swaggerDefinition: {
-		info: {
-			title: packageJson.name,
-			version: packageJson.version,
-		},
-	},
-	apis: glob.sync("server/routes/*.js", {
-		absolute: true,
-	}),
-};
 
 // Set config for Swagger UI
 const config = {
@@ -38,9 +22,9 @@ const config = {
 	},
 };
 
-module.exports = () => {
+module.exports = (swaggerData) => {
 	return new Promise((resolve, reject) => {
-		prettySwag.run(swaggerJSDoc(options), null, config, (err, data) => {
+		prettySwag.run(swaggerData, null, config, (err, data) => {
 			if (err) { // Something went wrong while creating the docs
 				reject(new Error("ERR_CREATING_DOCS"));
 			} else { // Everything is fine, send html
