b8c1398c0c3614d9c13ef5d3b0d46c9a76bae535
Thomas Bormans <thomas.bormans@district01.be>
2017-09-28 08:26:25 +0200

    Environments moved to config file

--- server/helpers/environment.js
+++ server/helpers/environment.js
@@ -1,13 +1,12 @@
+// We cannot require `config` because this requires the environment config
+// And we are checking if the environment config is valid... :)
+const generalConfig = require(process.cwd() + "/config/general");
 const Joi = require("joi");
 
 module.exports = (env) => {
 	// Validate env
 	const schema = Joi.object().keys({
-		NODE_ENV: Joi.string().required().valid([
-			"local",
-			"development",
-			"test",
-		]),
+		NODE_ENV: Joi.string().required().valid(generalConfig.server.environments),
 	});
 
 	const { error } = Joi.validate(env, schema, {
