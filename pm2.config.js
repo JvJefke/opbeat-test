0df58d15008e61ea31fff305a26f06a772b87b68
Thomas Bormans <thomas.bormans@district01.be>
2018-01-17 09:39:08 +0100

    Ignore camelcase in pm2 config file

--- pm2.config.js
+++ pm2.config.js
@@ -1,23 +1,24 @@
 const packageJson = require("./package.json");
 
+/* eslint-disable camelcase */
 module.exports = {
 	apps: [{
 		name: packageJson.name,
 		script: "./index.js",
 		watch: false,
 		instances: 2,
-		exec_mode: "cluster", // eslint-disable-line camelcase
-		log_date_format: "YYYY-DD-MM HH:mm:ss.SSS", // eslint-disable-line camelcase
+		exec_mode: "cluster",
+		log_date_format: "YYYY-DD-MM HH:mm:ss.SSS",
 		env: {
 			NODE_ENV: "local",
 		},
-		env_development: { // eslint-disable-line camelcase
+		env_development: {
 			NODE_ENV: "development",
 		},
-		env_staging: { // eslint-disable-line camelcase
+		env_staging: {
 			NODE_ENV: "staging",
 		},
-		env_production: { // eslint-disable-line camelcase
+		env_production: {
 			NODE_ENV: "production",
 		},
 	}],
